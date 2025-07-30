85e29c7e87608aa7cc05dc74e5edd0e3ea1cf325
Arun Prakash <230701037@rajalakshmi.edu.in>
2025-07-30 12:10:07 +0530

    Fix image path issue

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import {about} from './public/images/about-image.svg';
+import {about} from './assets/about-image.svg';
 
 gsap.registerPlugin(ScrollTrigger);
 
