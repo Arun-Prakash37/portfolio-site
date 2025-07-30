d8a1e93bc13ebe8a961295d348cd93fe583b6e70
Arun Prakash <230701037@rajalakshmi.edu.in>
2025-07-30 16:36:31 +0530

    Fix image paths for GitHub Pages

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import gsap from "gsap";
-import profileImage from '../assets/profile.jpg';
+import profileImage from '../assets/profile.JPG';
 
 const Hero = () => {
   // Refs for each animated element
